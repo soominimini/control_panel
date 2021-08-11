9baeba5b0ba286a81793f8290d243c87f2c4d04f
soominimini <lovesoomindl@naver.com>
2021-08-11 18:50:00 +0900

    ''

--- charger.py
+++ charger.py
@@ -7,6 +7,5 @@ import anki_vector
 
 if __name__ == '__main__':
     with anki_vector.Robot() as robot:
-        print("hi")
         print("Drive Vector onto charger...")
         robot.behavior.drive_on_charger()
